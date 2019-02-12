6ec471faec9691c126ce5a4b38a21f34d32bd63e
petek157 <petek157@Peter-Korugas-MacBook-Pro-1625.local>
2019-02-12 15:26:26 -0600

    Initial Commit

---
+++ src/DadsHouseBasementTemp.ino
@@ -0,0 +1,76 @@
+/*
+ * Project dadsHouseAP
+ * Description:
+ * Author:
+ * Date:
+ */
+
+#include "Adafruit_MCP9808.h"
+
+
+Adafruit_MCP9808 tempsensor = Adafruit_MCP9808();
+
+int currentTemp;
+int offset;
+int warningSent = false;
+int lowPoint = 40;
+
+unsigned long lastMillis = 0;
+unsigned long delayMillis = 1000 * 60 * 5; //5 minutes
+
+void setup() {
+
+  Particle.variable("temp", currentTemp);
+  Particle.variable("offset", offset);
+  Particle.variable("lowPoint", lowPoint);
+
+  Particle.function("chgOffset", changeOffset);
+  Particle.function("chgLowPoint", changeLowPoint);
+  
+  if (!tempsensor.begin()) {
+    while (1);
+  }
+
+}
+
+void loop() {
+
+unsigned long currentMillis = millis();
+
+  if (lastMillis == 0 || (currentMillis - lastMillis) > delayMillis) {
+    lastMillis = currentMillis;
+
+    checkTemp();
+
+  }
+
+}
+
+void checkTemp() {
+  float temp = (((tempsensor.readTempC() * 1.8) + 32) + offset);
+  
+  if (temp > 0 && temp < 100) {
+    if (temp > (currentTemp + 3) || temp < (currentTemp - 3)) {
+      currentTemp = temp;
+      if (temp < lowPoint && !warningSent) {
+        //Send warning about low temperature
+        warningSent = true;
+      }
+      if (temp > (lowPoint + 3) && warningSent) {
+        //consider an alert the says temp has recovered above low point
+        warningSent = false;
+      }
+    }
+  }
+  
+}
+
+int changeOffset(String n_offset) {
+  offset = n_offset.toInt();
+  return 1;
+}
+
+int changeLowPoint(String n_lowPoint) {
+  lowPoint = n_lowPoint.toInt();
+  return 1;
+}
